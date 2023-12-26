c69d2ecce1f74c01c842d619a21b4a154414857f
AppsTests <154893578+AppsTests@users.noreply.github.com>
2023-12-26 17:33:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! It is the best app in the world to say Hello World!')
 })
 
 app.listen(port, () => {
